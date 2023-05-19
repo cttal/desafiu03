81fcd74aa125921ecb16e06930f9b0d876ba18fd
predo araujo <129328323+cttal@users.noreply.github.com>
2023-05-19 08:58:17 -0300

    Add files via upload

---
+++ codes.js
@@ -0,0 +1,15 @@
+function ativarNome(){
+    let nome = 'predinpredao';
+    console.log(nome)
+}
+
+
+function somarNumeros(){
+    let a = 23;
+    let b = 12;
+
+
+    let c = a*b;
+
+    console.log(c)
+}
